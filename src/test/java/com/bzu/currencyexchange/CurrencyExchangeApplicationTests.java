22f36a7e3fce5716e1073d4041cb1b28f61e1b8c
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2023-05-31 07:58:56 +0300

    Create WebPage

--- src/test/java/com/bzu/currencyexchange/CurrencyExchangeApplicationTests.java
+++ src/test/java/com/bzu/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -1,9 +1,7 @@
 package com.bzu.currencyexchange;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CurrencyExchangeApplicationTests {
 
     @Test
